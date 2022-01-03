37682bdb128d402d80ed510fb784f4854603d2d6
Dima <55049959+Dimonitr@users.noreply.github.com>
2022-01-03 22:34:02 +0200

    Add files via upload

---
+++ Strob_Refresh/Strob_Refresh.ino
@@ -0,0 +1,82 @@
+int button = 2, v = 0, a = 0, a0 = 0;
+//long ar[100], ar2[100], ch[100];
+int led = 9;
+long long delay_mks = 10, d = 0, c = 0;
+long k = 0, kv = 0, kn = 0, kp = 0, kz = 0;
+char port1 = 0;
+
+
+
+
+
+bool mas[256] = {0};
+int pos = -1;
+byte tmp;
+ 
+
+
+
+
+
+
+
+
+
+
+void setup() {
+
+pinMode(led, OUTPUT);
+
+pinMode(button, INPUT);
+  pinMode(A0, OUTPUT); digitalWrite(A0, 0);
+  pinMode(A1, INPUT);
+  pinMode(A2, OUTPUT); digitalWrite(A2, 1);
+  pinMode(13, OUTPUT);
+  pinMode(1, INPUT);
+   Serial.begin(9600);
+}
+
+int gate = 0, key = 0;
+int ar[5]; 
+
+void loop() {
+  if(Serial.available() > 0)
+  {
+      port1 = Serial.read();
+      if((port1 == 'o')&(gate == 0)){
+          gate = 1;
+          key = 0;
+      }
+      else
+      {
+          if((port1 != 'o')&(gate == 1)&(key<4)&(port1 != 'e')){
+              ar[k] = port1 - 48; 
+              key++;
+              digitalWrite(13, LOW);
+              delay(1000);
+              digitalWrite(13, HIGH);
+          }
+          else
+          {
+              if(port1 == 'e')
+              {
+                  gate = 0;
+              digitalWrite(13, LOW);
+              delay(100);
+              digitalWrite(13, HIGH);
+              }    
+          }
+       }
+  }
+   if(key == 4){
+       d = ar[0]*10000+ar[1]*1000+ar[2]*100+ar[3]*10+ar[4];
+       key = 0;
+       digitalWrite(13,LOW);
+       delay(500);
+       digitalWrite(13, HIGH);
+   }
+   digitalWrite(13,1);
+   delay(100);
+   digitalWrite(13,0);
+   delay(d);
+}
---
+++ Stroboscop_Project/Stroboscop_Project/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Stroboscop_Project
+{
+    public partial class Form1 : Form
+    {
+
+        int db, dl, dt, dr;
+
+        bool updated = false;
+        void thr()
+        {
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+
+
+            bool gate = false;
+
+            int k = 0;
+
+            while (true)
+            {
+                string s = serialPort1.ReadLine();
+                if(s != "")
+                {
+                    listBox1.Invoke(new Action(() => listBox1.Items.Add(s)));
+                    if (s == "o")
+                    {
+                        gate = true;
+                    }
+                    if (s == "c")
+                    {
+                        gate = false;
+                    }
+
+                    if (gate)
+                    {
+                        updated = false;
+                        try
+                        {
+                            int n = int.Parse(s);
+                            if (k == 0)
+                            {
+                                db = n;
+                            }
+                            else
+                            {
+                                if (k == 1)
+                                {
+                                    dl = n;
+                                }
+                                else
+                                {
+                                    if (k == 2)
+                                    {
+                                        dt = n;
+                                    }
+                                    else
+                                    {
+                                        if (k == 3)
+                                        {
+                                            dr = n;
+                                        }
+                                    }
+                                }
+                            }
+                            k++;
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        updated = true;
+                    }
+                }
+            }
+        }
+
+        Thread tread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tread = new Thread(thr);
+            tread.Start();
+            timer1.Interval = 200;
+            timer1.Start();
+        }
+
+        bool gate = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = db.ToString();
+            label3.Text = dl.ToString();
+            label4.Text = dt.ToString();
+            label5.Text = dr.ToString();
+        }
+
+        string[] ser = new string[9];
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tread.Abort();
+        }
+
+        private void btLaunch_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
